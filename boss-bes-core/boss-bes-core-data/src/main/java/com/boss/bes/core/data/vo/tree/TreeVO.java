49ec9648ccfdcf923e29b6e87fc7b29939a62249
Bylight <hejiaweicn@163.com>
2019-08-22 09:40:32 +0800

    新增树结构相关VO

---
+++ boss-bes-core/boss-bes-core-data/src/main/java/com/boss/bes/core/data/vo/tree/TreeVO.java
@@ -0,0 +1,36 @@
+package com.boss.bes.core.data.vo.tree;
+
+
+import com.boss.bes.core.data.vo.tree.node.AbstractTreeNode;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * 树结构的VO对象，使用时需要指定节点的类型
+ *
+ * @author 何家伟
+ * @version 1.0
+ * @date 2019-08-21 15:30
+ */
+public class TreeVO<T extends AbstractTreeNode> {
+    @Valid
+    @NotNull(message = "树节点不能为空")
+    private List<T> treeNodeList;
+
+    public List<T> getTreeNodeList() {
+        return treeNodeList;
+    }
+
+    public void setTreeNodeList(List<T> treeNodeList) {
+        this.treeNodeList = treeNodeList;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeVO{" +
+                "treeNodeList=" + treeNodeList +
+                '}';
+    }
+}
---
+++ boss-bes-core/boss-bes-core-data/src/main/java/com/boss/bes/core/data/vo/tree/node/AbstractTreeNode.java
@@ -0,0 +1,58 @@
+package com.boss.bes.core.data.vo.tree.node;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * 所有树节点都需要继承的抽象类
+ * 继承的时候必须重写toString方法
+ *
+ * @author 何家伟
+ * @version 1.0
+ * @date 2019-08-21 15:23
+ */
+public abstract class AbstractTreeNode<T extends AbstractTreeNode> {
+    @NotNull(message = "树节点id不能为空")
+    @Min(value = 1, message = "树节点id必须大于0")
+    private Integer id;
+    @NotBlank(message = "节点名称不能为空")
+    private String name;
+
+    private List<T> childList;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<T> getChildList() {
+        return childList;
+    }
+
+    public void setChildList(List<T> childList) {
+        this.childList = childList;
+    }
+
+
+    @Override
+    public String toString() {
+        return "AbstractTreeNode{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", childList=" + childList +
+                '}';
+    }
+}
